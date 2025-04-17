8b91b46494d38b00be38c622a230f525d3e69647
Yigitcanozsahin <135243284+Yigitcanozsahin@users.noreply.github.com>
2025-04-17 23:17:05 +0300

    .

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -5,11 +5,10 @@ import PropTypes from "prop-types";
 import { useContext, useState } from "react";
 import { CartContext } from "../../context/CartProvider";
 import "./ProductItem.css";
-import { Link } from "react-router-dom";
 import ProductDetailsModal from "../ProductDetails/ProductDetailsModal";
 
 const ProductItem = ({ productItem }) => {
-  const { cartItems, addToCart } = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
 
   const filteredCart = cartItems.find(
     (cartItem) => cartItem._id === productItem._id
@@ -20,29 +19,34 @@ const ProductItem = ({ productItem }) => {
   const discountedPrice =
     originalPrice - (originalPrice * discountPercentage) / 100;
 
-  // Modal görünürlük state
   const [isModalVisible, setIsModalVisible] = useState(false);
 
-  // Göz ikonuna tıklanınca modal aç
   const handleOpenModal = () => {
     setIsModalVisible(true);
   };
 
-  // Modal kapanınca
   const handleCloseModal = () => {
     setIsModalVisible(false);
   };
 
+  const handleCardClick = () => {
+    window.location.href = `/product/${productItem._id}`;
+  };
+
   return (
     <>
-      <div className="product-item glide__slide glide__slide--active">
-        {/* Ürün görselleri */}
-        <div className="product-image">
-          <Link to={`/product/${productItem._id}`}>
-          <img src={productItem.img[0]} alt="" />
-            {/* <img src={productItem.img[0]} alt="" className="img1" />
-            <img src={productItem.img[1]} alt="" className="img2" /> */}
-          </Link>
+      <div
+        className="product-item glide__slide glide__slide--active"
+        onClick={handleCardClick}
+      >
+        <div
+          className="product-image"
+          onClick={(e) => {
+            e.stopPropagation(); // Kart yönlendirmesini engelle
+            handleOpenModal();
+          }}
+        >
+          <img src={productItem.img[0]} alt={productItem.name} />
         </div>
 
         <div className="product-info">
@@ -50,67 +54,26 @@ const ProductItem = ({ productItem }) => {
             {productItem.name}
           </a>
           <ul className="product-star">
-            <li>
-              <i className="bi bi-star-fill"></i>
-            </li>
-            <li>
-              <i className="bi bi-star-fill"></i>
-            </li>
-            <li>
-              <i className="bi bi-star-fill"></i>
-            </li>
-            <li>
-              <i className="bi bi-star-fill"></i>
-            </li>
-            <li>
-              <i className="bi bi-star-half"></i>
-            </li>
+            <li><i className="bi bi-star-fill"></i></li>
+            <li><i className="bi bi-star-fill"></i></li>
+            <li><i className="bi bi-star-fill"></i></li>
+            <li><i className="bi bi-star-fill"></i></li>
+            <li><i className="bi bi-star-half"></i></li>
           </ul>
           <div className="product-prices">
             <strong className="new-price">${discountedPrice.toFixed(2)}</strong>
             <span className="old-price">${originalPrice.toFixed(2)}</span>
           </div>
           <span className="product-discount">
-            -{productItem.price.discount}%
+            -{discountPercentage}%
           </span>
-
-          <div className="product-links">
-            {/* Sepete ekle butonu */}
-            <button
-              className="add-to-cart"
-              onClick={() =>
-                addToCart({
-                  ...productItem,
-                  price: discountedPrice,
-                })
-              }
-              disabled={filteredCart}
-            >
-              <i className="bi bi-basket-fill"></i>
-            </button>
-
-            <button>
-              <i className="bi bi-heart-fill"></i>
-            </button>
-
-            {/* Göz ikonu: Modal açar */}
-            <button className="product-link" onClick={handleOpenModal}>
-              <i className="bi bi-eye-fill"></i>
-            </button>
-
-            <a href="#!">
-              <i className="bi bi-share-fill"></i>
-            </a>
-          </div>
         </div>
       </div>
 
-      {/* Ürün Detay Modal */}
       <ProductDetailsModal
         isVisible={isModalVisible}
         onClose={handleCloseModal}
-        singleProduct={productItem} 
-        // => DİKKAT: normalde productItem'da full data (description, vs.) var mı?
+        singleProduct={productItem}
         setSingleProduct={() => {}}
       />
     </>
